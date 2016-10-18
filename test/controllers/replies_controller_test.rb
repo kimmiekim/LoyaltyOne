4762bef6cdf55d560e9f4db8f3787dae61fbd1ef
Erin Kim <kim.erin.kim@gmail.com>
2016-10-17 22:52:26 -0400

    Reply unit tests, changing back to button unit tests

--- test/controllers/replies_controller_test.rb
+++ test/controllers/replies_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class RepliesControllerTest < ActionController::TestCase
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
+  test "posting to create increases the model count" do
+    assert_difference("Reply.count") do
+      post :create, { :text_id => Text.last.id, :reply_text => "replies", :username => "bobby the replier" }
+    end
+  end
 
+  test "posting to create returns the right number of replies to the text" do
+    post :create, { :text_id => Text.last.id, :reply_text => "replies", :username => "bobby the replier" }
+
+    assert_equal Text.last.replies.count, JSON.parse(@response.body).length
+  end
+
+  test "post content is equal to rendered content" do
+    testText = "hello"
+    testUsername = "bobby"
+    post :create, { :text_id => Text.last.id, :reply_text => testText, :username => testUsername }
+    assert_equal testText, Reply.last.reply_text
+    assert_equal testUsername, Reply.last.username
+  end
 end
