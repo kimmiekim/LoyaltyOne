9e7c15ca75d06d66c2d987776f31278d8b5c9931
Erin Kim <kim.erin.kim@gmail.com>
2016-10-19 21:05:34 -0400

    Adding in replies controller code from step6

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,16 @@
 class RepliesController < ApplicationController
   def create
+    # Get the text to build the reply for
+    text_id = params[:text_id]
+    text = Text.find_by_id(text_id)
+
+    # build the reply
     user_input = params[:reply_text]
     username = params[:username]
-    text_id = params[:text_id]
+    reply = text.replies.build(reply_text: user_input, username: username)
+    reply.save
 
-    Reply.create(reply_text: user_input, username: username, text_id: text_id)
+    # Return full list of json replies related to text
+    render json: text.replies
   end
 end
