8cd1f8b3b78a5412441ad2635613e03172ee72d4
Erin Kim <kim.erin.kim@gmail.com>
2016-10-19 21:04:24 -0400

    bringing in replies code for app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,37 +11,46 @@ myApp.controller('mainController', [
       $event.preventDefault();
     }
 
-    vm.sendText = function (text, username, address, $event){
+    vm.sendText = function (text, username, $event){
       $event.preventDefault();
 
       $http({
         method: 'POST',
         // this sends the data to texts path without username
-        // url: '/texts',
         url: '/create',
         // this sends data with username
-        data: { text: text, username: username, address: address }
+        data: { text: text, username: username }
       }).then(function successCallback(response) {
         vm.textList = response.data;
         vm.latest = vm.textList[0];
-        console.log(vm.textList);
-        console.log(vm.latest);
       }, function errorCallback(response) {
       });
     }
 
-    vm.sendReply = function (reply_text, username, text_id, $event){
+    vm.sendReply = function (text, $index, $event){
       $event.preventDefault();
+
+      // Save the index in the controller so that the index can be used
+      vm.text_index = $index;
 
       $http({
         method: 'POST',
         // this sends the data to texts path without username
-        // url: '/texts',
         url: '/reply/create',
-        // this sends data with username
-        data: { reply_text: reply_text, username: username, text_id: text_id }
-      }).then(function successCallback(response) {
+        data: { 
+          reply_text: text.reply_text,
+          username: text.reply_username,
+          text_id: text.id }
+      }).then(function successCallback(response, $index, indexHolder) {
+        var text_index = vm.text_index;
+        var textObj = vm.textList[vm.text_index];
+        // Refresh the list of replies on the specific text object
+        vm.textList[text_index].replies = response.data;
 
+        // Clear the form
+        vm.textList[text_index].reply_text = undefined;
+        vm.textList[text_index].reply_username = undefined;
+        
       }, function errorCallback(response) {
       });
     }
