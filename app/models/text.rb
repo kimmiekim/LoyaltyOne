dffa8ad9c4e6f1c1c78ee71bc4d77f7b19e8492b
Erin Kim <kim.erin.kim@gmail.com>
2016-10-26 22:03:53 -0400

    changed text association with weather info to be able to delete all texts on heroku

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,6 +1,6 @@
 class Text < ActiveRecord::Base
   has_many :replies, dependent: :destroy
-  has_one :weather_info
+  has_one :weather_info, dependent: :destroy
   geocoded_by :address
   after_validation :geocode
 end
